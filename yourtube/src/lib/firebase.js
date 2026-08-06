e28be1331a5bad58ae0878c542a212bc92336623
Aayush0910-glitch <aayush.p.chauhan24@slrtce.in>
2026-08-07 00:53:19 +0530

    fix: update firebase config to user project

--- yourtube/src/lib/firebase.js
+++ yourtube/src/lib/firebase.js
@@ -6,12 +6,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCyxbdclt2ocA5zgE-MDy1ndYIFqVMAr30",
-  authDomain: "yourtube-8cda9.firebaseapp.com",
-  projectId: "yourtube-8cda9",
-  storageBucket: "yourtube-8cda9.firebasestorage.app",
-  messagingSenderId: "921641878423",
-  appId: "1:921641878423:web:0d65801eebaf2b25f03ad2",
+  apiKey: "AIzaSyCWVdRqX0_A0xYq3P6yg24r-4iCxXVOK_Q",
+  authDomain: "clone-7241b.firebaseapp.com",
+  projectId: "clone-7241b",
+  storageBucket: "clone-7241b.firebasestorage.app",
+  messagingSenderId: "1044189493632",
+  appId: "1:1044189493632:web:6966b813771d7cf054e965",
+  measurementId: "G-RF3ND66MN4"
 };
 
 // Initialize Firebase
